0d1c8f103ad3bd8efa5d61fc2e270a28eca854e2
Ealon1996 <38780025+Ealon1996@users.noreply.github.com>
2018-07-01 02:44:04 +0800

    載入套件與訓練資料

---
+++ HandWriting.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul  1 02:41:11 2018
+
+@author: s7856
+"""
+
+import numpy as np 
+from keras.utils import np_utils
+from keras.datasets import mnist 
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+X_train = X_train.reshape(-1, 1,28, 28)/255.
+X_test = X_test.reshape(-1, 1,28, 28)/255.
+y_train = np_utils.to_categorical(y_train, num_classes=10)
+y_test = np_utils.to_categorical(y_test, num_classes=10)
